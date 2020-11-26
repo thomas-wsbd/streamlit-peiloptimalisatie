195fb27cd4719fc3a373999cbe7de31ba2955fa4
Thomas <t.deurloo@brabantsedelta.nl>
2020-11-26 14:17:02 +0100

    widescreen mode default

--- app.py
+++ app.py
@@ -10,6 +10,21 @@ from vzm.utilities import *
 
 COL_VZM = "VZM_WT"
 
+def _max_width_():
+    max_width_str = f"max-width: 2000px;"
+    st.markdown(
+        f"""
+    <style>
+    .reportview-container .main .block-container{{
+        {max_width_str}
+    }}
+    </style>    
+    """,
+        unsafe_allow_html=True,
+    )
+
+_max_width_()
+
 @st.cache(allow_output_mutation=True)
 def load_abt():
     # Load the data
